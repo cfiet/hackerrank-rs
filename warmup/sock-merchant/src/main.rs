0896ec1b34537443f8b55856d1b22e1c52f4d52b
Maciej Makowski <maciejm.githubatcfiet.net>
2019-12-27 19:36:41 +0000

    Adding tests

--- warmup/sock-merchant/src/main.rs
+++ warmup/sock-merchant/src/main.rs
@@ -53,16 +53,34 @@ where
         .map(Vec::into_boxed_slice)
 }
 
-fn main() {
-    let mut stdin = std::io::stdin();
-    let socks: Box<[i32]> = read_input(&mut stdin).unwrap();
-
+fn count_pairs(socks: &[i32]) -> usize {
     let mut by_colors = BTreeMap::<i32, usize>::new();
     socks.iter().for_each(|v| {
         let val: &mut usize = by_colors.entry(*v).or_insert(0);
         *val += 1;
     });
 
-    let pairs: usize = by_colors.values().map(|v| v / 2).sum();
-    print!("{}", pairs);
+    by_colors.values().map(|v| v / 2).sum()
+}
+
+fn main() {
+    let mut stdin = std::io::stdin();
+    let socks: Box<[i32]> = read_input(&mut stdin).unwrap();
+
+    print!("{}", count_pairs(&socks));
+}
+
+#[cfg(test)]
+mod test {
+    use std::fs::File;
+
+    use super::*;
+
+    #[test]
+    fn sock_merchant_example_data() {
+        let mut input = File::open("data/test-input.txt").unwrap();
+        let socks: Box<[i32]> = read_input(&mut input).unwrap();
+
+        assert_eq!(count_pairs(&socks), 3);
+    }
 }
